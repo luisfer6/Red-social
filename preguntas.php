c79183491a629f6d16ae0a89b99fe0e6ff42fa04
mathias121993 <56778508+mathias121993@users.noreply.github.com>
2019-11-12 16:34:16 -0300

    Etapa1

---
+++ preguntas.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<?=!include('header.php'); ?>
+
+  <div class="container">
+  	  	<div class="row">
+  	  		<div class="col-lg-5 col-sm-12 text-center">
+            
+              <div class="row pregunta">
+                <!--PREGUNTA-->
+                <p class="col-lg-12 consigna"><span>?</span>Lorem ipsum dolor sit amet, consectetur adipisicing
+                </p>
+                 <p class=" col-lg-12 respuesta">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                </p>
+                <!--PREGUNTA-->
+                <p class="col-lg-12 consigna"><span>?</span>Lorem ipsum dolor sit amet, consectetur adipisicing
+                </p>
+                 <p class=" col-lg-12 respuesta">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                </p>
+                <!--PREGUNTA-->
+                <p class="col-lg-12 consigna"><span>?</span>Lorem ipsum dolor sit amet, consectetur adipisicing
+                </p>
+                 <p class=" col-lg-12 respuesta">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                </p>
+                <!--PREGUNTA-->
+                <p class="col-lg-12 consigna"><span>?</span>Lorem ipsum dolor sit amet, consectetur adipisicing
+                </p>
+                 <p class=" col-lg-12 respuesta">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                </p>
+                <!--PREGUNTA-->
+                <p class="col-lg-12 consigna"><span>?</span>Lorem ipsum dolor sit amet, consectetur adipisicing
+                </p>
+                 <p class=" col-lg-12 respuesta">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                </p>
+                
+               
+
+              </div>
+              
+
+             
+          </div>
+      			<div class="col-lg-7 d-none d-lg-block centro ">
+      				<img class="img-fluid" src="img/logop1.png">
+      			</div>
+  	  	</div>
+  	
+  </div>
+
+
+<?=!include('footer.php'); ?>
+
+</body>
+</html>
