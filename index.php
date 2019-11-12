993816f9726492d0ef4aadda95cd9e06aa542f4b
Federico <federicomargueliche@gmail.com>
2019-11-12 18:40:14 -0300

    Agrego el archivo index.php, el cual es el mismo que index3.php que estaba anteriormente

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<?=!include('header.php'); ?>
+
+  <div class="container">
+  	  	<div class="row">
+  	  		<div class="col-lg-5 col-sm-12 centro">
+  				<section>
+  					<img src="img/title.png" class="img-fluid">
+  					<article class="text-center"><p class="blanco">Sitio de reunion para programadores, estudiantes , reclutadores y empresas IT</p></article>
+  				</section>
+  			</div>
+  			<div class="col-lg-7 d-none d-lg-block centro ">
+  				<img class="img-fluid" src="img/logop1.png">
+  			</div>
+  	  	</div>
+  	
+  </div>
+
+
+<?=!include('footer.php'); ?>
+
+</body>
+</html>
